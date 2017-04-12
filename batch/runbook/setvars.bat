39c5552f07ae478ddbfb8b3df8956af4c537212b
pbuditi <pbuditi@gmail.com>
2017-04-12 17:40:22 +0400

    Create setvars.bat

---
+++ batch/runbook/setvars.bat
@@ -0,0 +1,7 @@
+set _version="16.6.0"
+set _runbookname="runbook_main.txt"
+set _owsdb=10.119.5.115:1550/way4bof1
+set _owspwd=Discovery#3
+set _owsapwd=Discovery#4
+set _owshome="d:\ows_home\OWS_HOME_03.43.30.26\"
+set _owswork="y:\OWS_WORKS\prf\w4bof01\"
